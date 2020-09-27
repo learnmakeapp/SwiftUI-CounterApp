7f884b86252a7bc510f1cd5631afd12434f217ed
Arifin Firdaus <arifin.firdaus1996@gmail.com>
2020-09-27 10:26:37 +0700

    add base design

--- LMACounter/ContentView.swift
+++ LMACounter/ContentView.swift
@@ -9,9 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center, spacing: 112) {
+            makeCountText()
+            HStack(alignment: .center, spacing: 64) {
+                makeResetButton()
+                makeCountButton()
+            }
+        }
     }
+    
+    fileprivate func makeCountText() -> Text {
+        return Text("33")
+            .font(Font.system(size: 68))
+    }
+    
+    fileprivate func makeResetButton() -> some View {
+        return Button(action: {
+            print("reset button tapped")
+        }) {
+            Text("Reset")
+                .foregroundColor(.white)
+                .bold()
+        }
+        .frame(width: 130, height: 50, alignment: .center)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke())
+        .background(Color.red)
+        .cornerRadius(8)
+    }
+    
+    fileprivate func makeCountButton() -> some View {
+        return Button(action: {
+            print("count button tapped")
+        }) {
+            Text("Count")
+                .foregroundColor(.white)
+                .bold()
+        }
+        .frame(width: 130, height: 50, alignment: .center)
+        .overlay(RoundedRectangle(cornerRadius: 8).stroke())
+        .background(Color.blue)
+        .cornerRadius(8)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
